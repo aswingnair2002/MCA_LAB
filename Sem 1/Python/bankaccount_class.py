480ed6ed8f25c05ab6c5c95b078fd84d51c1da5c
AKASH P GANESH <akash.mca23@mbits.ac.in>
2023-11-25 11:10:11 +0530

    25-11-2023

--- Sem 1/Python/bankaccount_class.py
+++ Sem 1/Python/bankaccount_class.py
@@ -4,3 +4,45 @@ class BankAccount:
 		self.account_holder_name = account_holder_name
 		self.account_type = account_type
 		self.balance = balance
+		
+	def deposit(self, amount):
+		if amount > 0:
+			self.balance += amount
+			print(f"Deposit of ${amount} successful. New balance: ${self.balance}")
+		else:
+			print("Invalid deposit amount. Please enter a positive value.")
+	
+	def withdraw(self,amount):
+		if 0 < amount <= self.balance:
+			self.balance -= amount
+			print(f"Withdrawal of ${amount} successful. New balance: ${self.balance}")
+		else:
+			print("Invalid withdrawal amount or insufficient funds.")
+	
+	def display_account_info(self):
+		print(f"Account Number: {self.account_number}")
+		print(f"Account Holder: {self.account_holder_name}")
+		print(f"Account Type: {self.account_type}")
+		print(f"Current Balance: ${self.balance}")
+		
+account1 = BankAccount("123456","John Doe","Savings",1000.0)
+account1.display_account_info()
+account1.deposit(500.0)
+account1.withdraw(200.0)
+account1.display_account_info()
+
+
+'''
+Output:
+
+Account Number: 123456
+Account Holder: John Doe
+Account Type: Savings
+Current Balance: $1000.0
+Deposit of $500.0 successful. New balance: $1500.0
+Withdrawal of $200.0 successful. New balance: $1300.0
+Account Number: 123456
+Account Holder: John Doe
+Account Type: Savings
+Current Balance: $1300.0
+'''
